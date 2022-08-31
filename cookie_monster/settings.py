f69eb5acaa173cd86adb6338ee9702e18fe731e8
Petter Kvalvaag <pmkv@equinor.com>
2022-08-31 22:12:33 +0200

    Use MariaDB in prod

--- cookie_monster/settings.py
+++ cookie_monster/settings.py
@@ -78,12 +78,25 @@ WSGI_APPLICATION = 'cookie_monster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if ENVIRONMENT == 'prod':
+    DB_PASSWORD = os.environ.get('MARIADB_PASSWORD')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'cookiemonster',
+            'USER': 'cookiemonster',
+            'PASSWORD': DB_PASSWORD,
+            'HOST': 'mariadb',
+            'PORT': '3306'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
