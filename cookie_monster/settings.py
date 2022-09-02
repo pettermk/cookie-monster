c41e8696826f7f2e8c4639e0e1909283310402b3
Petter Kvalvaag <pmkv@equinor.com>
2022-09-02 10:40:15 +0200

    RTFMed and put the whitenoise middleware correctly
    http://whitenoise.evans.io/en/stable/django.html#enable-whitenoise

--- cookie_monster/settings.py
+++ cookie_monster/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cookie_monster.urls'
